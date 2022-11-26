8e04178ecc6fd074b4e55bf908d0e08a1d2ac3d2
Lu <lokka.rei@yandex.ru>
2022-11-26 19:59:51 +0700

    насписала список команд и структуру ответов

--- src/main/java/pro/sky/telegrambot/constant/MessageUserAboutShelter.java
+++ src/main/java/pro/sky/telegrambot/constant/MessageUserAboutShelter.java
@@ -1,4 +1,13 @@
 package pro.sky.telegrambot.constant;
 
 public class MessageUserAboutShelter {
+    public final String DATING_RULES = "/dating_rules";
+    public final String DOCUMENTS_LIST = "/documents_list";
+    public final String TRANSPORT_RECOMMENDATIONS = "/transports_recommendations";
+    public final String HOME_IMPROVEMENT_PUPPY = "/home_improvement_puppy";
+    public final String HOME_IMPROVEMENT_DOG = "/home_improvement_dog";
+    public final String HOME_IMPROVEMENT_DISABLED_DOG = "/home_improvement_disabled_dog";
+    public final String CYNOLOGIST_TIPS = "/cynologist_tips";
+    public final String CYNOLOGISTS_LIST = "/cynologist_list";
+    public final String BOUNCE_LIST = "/bounce_list";
 }
