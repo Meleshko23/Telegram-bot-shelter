2aa012a08ddb442261b9bcd241a3573d003ce062
abdullinru <abdullin17021989@gmail.com>
2022-12-01 11:19:02 +0600

    обработка нажатия польз-ля на кнопку "инфо о приюте"

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -34,44 +34,73 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
-            if (update.message().text().equals("/start")) {
-                Long chatId = update.message().chat().id();
-                SendMessage sendMess = new SendMessage(chatId, "Привет!!! Хочешь взять собаку из приюта");
-
-                sendMess.replyMarkup(preparekeyboard());
-
-                SendResponse response = telegramBot.execute(sendMess);
-
+            if (update.message() != null) {
+                if (update.message().text().equals("/start")) {
+                    Long chatId = update.message().chat().id();
+                    responseOnCommandStart(chatId);
+                }
             }
+
+            else if (update.callbackQuery() != null) {
+                if (update.callbackQuery().data().equals("ONE")) {
+                    Long chatId = update.callbackQuery().message().chat().id();
+                    responseOnCommandInfoShelter(chatId);
+                }
+            }
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    private void responseOnCommandStart(long chatId) {
+
+        SendMessage sendMess = new SendMessage(chatId, "Привет!!! Хочешь взять собаку из приюта");
+        sendMess.replyMarkup(preparekeyboardStart());
+        SendResponse response = telegramBot.execute(sendMess);
+    }
+    private void responseOnCommandInfoShelter(long chatId) {
+
+        SendMessage sendMess = new SendMessage(chatId, "Нажми на кнопку ниже. Выбери то,что тебя интересует");
+        sendMess.replyMarkup(preparekeyboardInfoShelter());
+        SendResponse response = telegramBot.execute(sendMess);
+    }
     /**
-     * метод создает инлайн клавиатуру
+     * метод создает инлайн клавиатуру после отправки команды Start
      * @return клавиатура подсообщением
      */
-    private InlineKeyboardMarkup preparekeyboard() {
+    private InlineKeyboardMarkup preparekeyboardStart() {
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
-        List<InlineKeyboardButton> rowInline = new ArrayList<>();
-        InlineKeyboardButton button1 = new InlineKeyboardButton("1");
-        InlineKeyboardButton button2 = new InlineKeyboardButton("2");
-        InlineKeyboardButton button3 = new InlineKeyboardButton("3");
-        InlineKeyboardButton button4 = new InlineKeyboardButton("4");
 
-        button1.callbackData("One");
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Информация о приюте");
+        InlineKeyboardButton button2 = new InlineKeyboardButton("Информация о питомцах");
+        InlineKeyboardButton button3 = new InlineKeyboardButton("Взаимодействие с питомцем ");
+        InlineKeyboardButton button4 = new InlineKeyboardButton("Позвать волонтера");
+
+        button1.callbackData("ONE");
         button2.callbackData("TWO");
-        button3.callbackData("Three");
-        button4.callbackData("Forth");
+        button3.callbackData("THREE");
+        button4.callbackData("FORTH");
 
-//                rowInline.add(button1);
-//                rowInline.add(button2);
-//                rowsInline.add(rowInline);
-//
-//                rowInline.add(button3);
-//                rowInline.add(button4);
-//                rowsInline.add(rowInline);
+        markupInline.addRow(button1, button2);
+        markupInline.addRow(button3, button4);
+        return markupInline;
+    }
+    /**
+     * метод создает инлайн клавиатуру после отправки команды для получения информации о приюте
+     * @return клавиатура под сообщением
+     */
+    private InlineKeyboardMarkup preparekeyboardInfoShelter() {
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Контакты приюта");
+        InlineKeyboardButton button2 = new InlineKeyboardButton("График работы");
+        InlineKeyboardButton button3 = new InlineKeyboardButton("Что-то еще");
+        InlineKeyboardButton button4 = new InlineKeyboardButton("И еще что-то");
+
+        button1.callbackData("CONTACTS");
+        button2.callbackData("GRAPHIC");
+        button3.callbackData("qqqq");
+        button4.callbackData("wwww");
 
         markupInline.addRow(button1, button2);
         markupInline.addRow(button3, button4);
